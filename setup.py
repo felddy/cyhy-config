d5f4566d568e99b222285ad20ad0c3eef88ecc2a
Felddy <markf@geekpad.com>
2024-08-29 15:58:35 -0400

    Add py.typed file to allow users to type check

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
     keywords=["cyhy", "config"],
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    package_data={},
+    package_data={"cyhy_config": ["py.typed"]},
     py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     install_requires=[
