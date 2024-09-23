5624d094e448da3e34b838fc49325e8866913ab9
Felddy <markf@geekpad.com>
2024-09-23 16:45:16 -0400

    Update build from skeleton and limit Python to ≥3.11

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
