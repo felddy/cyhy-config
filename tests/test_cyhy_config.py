014df7e27736992236ca169b3785729f4cdbe58b
Felddy <markf@geekpad.com>
2024-09-24 19:14:07 -0400

    Add missing release test

--- tests/test_cyhy_config.py
+++ tests/test_cyhy_config.py
@@ -14,6 +14,7 @@ import pytest
 
 # cisagov Libraries
 from cyhy_config.cyhy_config import (
+    __version__,
     find_config_file,
     get_config,
     read_config_file,
@@ -26,6 +27,21 @@ class MockModel(BaseModel):
     """Mock model for testing."""
 
     key: str
+
+
+# define sources of version strings
+RELEASE_TAG = os.getenv("RELEASE_TAG")
+PROJECT_VERSION = __version__
+
+
+@pytest.mark.skipif(
+    RELEASE_TAG in [None, ""], reason="this is not a release (RELEASE_TAG not set)"
+)
+def test_release_version():
+    """Verify that release tag version agrees with the module version."""
+    assert (
+        RELEASE_TAG == f"v{PROJECT_VERSION}"
+    ), "RELEASE_TAG does not match the project version"
 
 
 def test_find_config_file_given_path_exists():
